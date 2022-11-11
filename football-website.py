750970841d17164a61f0d2738aa59a5f5a0ea0d3
Mohammed-Hamada <mohammed3rbio@gmail.com>
2022-11-11 15:47:43 +0200

    remove unneeded lines

--- football-website.py
+++ football-website.py
@@ -8,36 +8,9 @@ from bs4 import BeautifulSoup
 url = 'https://www.footballdatabase.eu'
 
 
-cookies = {
-    'trc_cookie_storage': 'taboola%2520global%253Auser-id%3D7966384e-086d-4bbe-a0ca-2d81259d2bc0-tucta22ed2a',
-    'PHPSESSID': 'pk93096s7qsqh6oob1o8lauo7u',
-    'fbdb_auth': '23f5e3f154aa0542d249d820c92fff79',
-    '_pbjs_userid_consent_data': '6683316680106290',
-    '_ga_31VFXW2CM3': 'GS1.1.1668163872.19.1.1668164505.0.0.0',
-    '_ga': 'GA1.2.789686284.1667945622',
-    '_gid': 'GA1.2.293859119.1668164506',
-    '_gat_gtag_UA_1896429_1': '1',
-    'cto_bidid': 'zTi4NF8xRTh1eFcxRmpWVWl6cVJOQlhTczBtUndyQnZhV0xXbTVRQ2wlMkZyaFFmTjZRRG5yQnklMkYzMUdmak40MFVFVzY5QmlBdnVXWENzSlBwQWdCTWJiNFVzTUV3QUFXYk9hamxxRFRKNGNNWFpMOExDVVY3YW1rUHpLQkNKTVdCNUxNcjQ',
-    'nadz_dailyVisits': '1',
-    'cto_bundle': 'VfxNHl9QNWVNWXRvbWZBUHczZjZVOUh5ME90QVg4T28xMU51TzJwRDhJUFpydHlicmE1eVd5c1lnNlNPWE1DTmNVJTJGd25aU240dVNwTThjeGg2cmtOWW5iZG12a0pMNEdSVTlwd0JuQ3pkaVI0bmFTS0FtS1JrY1NZVWYlMkY1ZGh1RVpqMEJNUnJJM29xQXY1M2laRVRmQWYlMkZiTnJGRWlyUXlkQVgxOTIlMkJ3MnlrM1BKTSUzRA',
-}
+cookies = {}
 
-headers = {
-    'authority': 'www.footballdatabase.eu',
-    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-    'accept-language': 'en-US,en;q=0.9',
-    # Requests sorts cookies= alphabetically
-    # 'cookie': 'trc_cookie_storage=taboola%2520global%253Auser-id%3D7966384e-086d-4bbe-a0ca-2d81259d2bc0-tucta22ed2a; PHPSESSID=pk93096s7qsqh6oob1o8lauo7u; fbdb_auth=23f5e3f154aa0542d249d820c92fff79; _pbjs_userid_consent_data=6683316680106290; _ga_31VFXW2CM3=GS1.1.1668163872.19.1.1668164505.0.0.0; _ga=GA1.2.789686284.1667945622; _gid=GA1.2.293859119.1668164506; _gat_gtag_UA_1896429_1=1; cto_bidid=zTi4NF8xRTh1eFcxRmpWVWl6cVJOQlhTczBtUndyQnZhV0xXbTVRQ2wlMkZyaFFmTjZRRG5yQnklMkYzMUdmak40MFVFVzY5QmlBdnVXWENzSlBwQWdCTWJiNFVzTUV3QUFXYk9hamxxRFRKNGNNWFpMOExDVVY3YW1rUHpLQkNKTVdCNUxNcjQ; nadz_dailyVisits=1; cto_bundle=VfxNHl9QNWVNWXRvbWZBUHczZjZVOUh5ME90QVg4T28xMU51TzJwRDhJUFpydHlicmE1eVd5c1lnNlNPWE1DTmNVJTJGd25aU240dVNwTThjeGg2cmtOWW5iZG12a0pMNEdSVTlwd0JuQ3pkaVI0bmFTS0FtS1JrY1NZVWYlMkY1ZGh1RVpqMEJNUnJJM29xQXY1M2laRVRmQWYlMkZiTnJGRWlyUXlkQVgxOTIlMkJ3MnlrM1BKTSUzRA',
-    'sec-ch-ua': '"Microsoft Edge";v="105", "Not)A;Brand";v="8", "Chromium";v="105"',
-    'sec-ch-ua-mobile': '?0',
-    'sec-ch-ua-platform': '"Linux"',
-    'sec-fetch-dest': 'document',
-    'sec-fetch-mode': 'navigate',
-    'sec-fetch-site': 'none',
-    'sec-fetch-user': '?1',
-    'upgrade-insecure-requests': '1',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36 Edg/105.0.1343.42',
-}
+headers = {}
 
 csv_players_columns = ['name', 'club', 'age', 'nationality', 'caps',
                        'height', 'weight', 'first cap', 'best foot', 'photo']
